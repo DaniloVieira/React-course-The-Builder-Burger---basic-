d7a2bd6567b67a49ed3c63b2d81a07a3984fb629
Danilo <methodic4u@gmail.com>
2020-05-04 13:35:38 -0300

    Redux advanced finished

--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../actions/actionsTypes';
+import { updateObject } from  '../utility';
 
 const initialState = {
     ingredients: null,
@@ -13,49 +14,45 @@ const INGREDIENTS_PRICES ={
     meat: 0.7
 }
 
-const reducer = (state = initialState, action) => {
+const ADD = 1;
+const REMOVE = -1;
 
+const toggleIngredient = (state, action, qtd) => {
+    const updatedIngredient = { [action.ingredientName]: state.ingredients[action.ingredientName] + qtd };
+    const updatedIngredients = updateObject(state.ingredients, updatedIngredient );
+    const updatedState = {
+        ingredients: updatedIngredients,
+        totalPrice: state.totalPrice + INGREDIENTS_PRICES[action.ingredientName]
+    }
+    return updateObject(state, updatedState);
+}
+
+const setIngredients = (state, action) => {
+    return updateObject(state, {
+        ingredients: {
+            salad: action.ingredients.salad,
+            bacon: action.ingredients.bacon,
+            cheese: action.ingredients.cheese,
+            meat: action.ingredients.meat
+        },
+        totalPrice: 4,
+        error: false
+    } )
+}
+
+const reducer = (state = initialState, action) => {
     switch (action.type) {
         case actionTypes.ADD_INGREDIENT:
-            return{
-                ...state,
-                ingredients: {
-                    ...state.ingredients,
-                    [action.ingredientName]: state.ingredients[action.ingredientName] + 1 // overwrites an ingredient
-                },
-                totalPrice: state.totalPrice + INGREDIENTS_PRICES[action.ingredientName]
-            }
+            return toggleIngredient(state, action, ADD);
         case actionTypes.REMOVE_INGREDIENT:
-            return{
-                ...state,
-                ingredients: {
-                    ...state.ingredients,
-                    [action.ingredientName]: state.ingredients[action.ingredientName] - 1 // overwrites an ingredient
-                },
-                totalPrice: state.totalPrice + INGREDIENTS_PRICES[action.ingredientName]
-            }
-
+            return toggleIngredient(state, action, REMOVE);
         case actionTypes.SET_INGREDIENTS:
-            return {
-                ...state,
-                ingredients: {
-                    salad: action.ingredients.salad,
-                    bacon: action.ingredients.bacon,
-                    cheese: action.ingredients.cheese,
-                    meat: action.ingredients.meat
-                },
-                totalPrice: 4,
-                error: false
-            }
+            return setIngredients(state, action);
         case actionTypes.FETCH_INGREDIENTS_FAILED:
-            return {
-                ...state,
-                error: true
-            }
+            return updateObject(state, {error: true});            
         default:
             return state;
     }
-
 };
 
 export default reducer;  
